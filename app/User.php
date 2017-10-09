c150630646d754085196468d5d46f118118d79df
Ihor Bondarenko <ihor.bondarenko@acropolium.com>
2017-10-09 16:08:14 +0300

    fixes

--- app/User.php
+++ app/User.php
@@ -4,18 +4,28 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laratrust\Traits\LaratrustUserTrait;
+use Webpatser\Uuid\Uuid;
 
 class User extends Authenticatable
 {
+    use LaratrustUserTrait;
     use Notifiable;
 
+    protected $primaryKey = 'uuid';
+
+    public $incrementing = false;
+
+    protected $casts = [
+      'uuid' => 'string'
+    ];
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+      'name', 'email', 'password', 'username'
     ];
 
     /**
@@ -24,6 +34,23 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'uuid', 'password', 'remember_token',
     ];
+
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function ($model) {
+            $model->uuid = (string) Uuid::generate(4);
+        });
+    }
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'uuid';
+    }
 }
